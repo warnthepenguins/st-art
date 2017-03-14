408b508ea13b94b934e18591aa1fa768affa6e98
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2017-03-14 13:37:01 -0400

    navbar

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_like, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /likes
   # GET /likes.json
